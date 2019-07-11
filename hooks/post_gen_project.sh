fa24189ae843ee62d457828125d14a65db76d2f3
Chris MacNaughton <chris@centaurisolutions.nl>
2019-07-11 08:39:48 +0200

    clean up hook gen

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+cd hooks
+for hook in install config-changed start stop upgrade-charm update-status; do ln -s hooks.py $hook; done
+cd ..
 git init
 git add .
 git commit -a -m "Initial Cookiecutter Commit."
